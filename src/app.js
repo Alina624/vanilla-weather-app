e7dffcbf8ed027ab17764c992e99f9e6debb9107
Alina624 <107378592+Alina624@users.noreply.github.com>
2022-08-14 19:31:43 -0500

    added new functions js

--- src/app.js
+++ src/app.js
@@ -22,6 +22,36 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <img
+                src="
+            https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt=""
+                width="36"
+              />
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperatures-max"> 20° </span>
+                <span class="weather-forecast-temperatures-min"> 12° </span>
+              </div>
+            </div>
+            
+`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -31,6 +61,8 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let dateEement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
+
+  displayForecast();
 
   celsiusTemperature = response.data.main.temp;
 
